fb3bdf34aad702e74d672ddb0e6164ea89dbfbbd
Jarlene <378389165@qq.com>
2016-05-20 18:49:36 +0800

    add method

--- httpclientlib/src/main/jni/HttpClient.cpp
+++ httpclientlib/src/main/jni/HttpClient.cpp
@@ -3,3 +3,62 @@
 //
 
 #include "HttpClient.h"
+
+HttpClient::HttpClient() {
+    if (!create()) {
+        _pipe_fd[0] = -1;
+        _pipe_fd[1] = -1;
+    }
+}
+
+HttpClient::~HttpClient() {
+    close();
+}
+
+
+bool HttpClient::write() {
+    if (_pipe_fd[1] == -1) {
+        return false;
+    }
+
+    int32_t ret = send(_pipe_fd[1], "a", 1, 0);
+    if (ret < 0) {
+        return false;
+    }
+
+    return true;
+}
+
+bool HttpClient::read() {
+    if (_pipe_fd[1] == -1) {
+        return false;
+    }
+    char buf[1024] = { 0 };
+    if (recv(_pipe_fd[0], buf, 1024, 0) <= 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+bool HttpClient::create() {
+    int32_t ret = socketpair(AF_UNIX, SOCK_STREAM, 0, _pipe_fd);
+    if (ret < 0) {
+        return false;
+    }
+    return true;
+}
+
+void HttpClient::close() {
+    close(_pipe_fd[0]);
+    close(_pipe_fd[1]);
+}
+
+int32_t HttpClient::get_listen_fd() {
+    return _pipe_fd[0];
+}
+
+void HttpClient::close(int32_t& fd) {
+    close(fd);
+    fd = -1;
+}
--- httpclientlib/src/main/jni/HttpClient.h
+++ httpclientlib/src/main/jni/HttpClient.h
@@ -5,11 +5,29 @@
 #ifndef HTTPCLIENT_HTTPCLIENT_H
 #define HTTPCLIENT_HTTPCLIENT_H
 
-#include <pthread.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 
 
 class HttpClient {
+
+public:
+    HttpClient();
+    virtual ~HttpClient();
+public:
+    bool write();
+    bool read();
+    void close();
+    int32_t get_listen_fd();
+
+protected:
+    bool create();
+    void close(int32_t& fd);
+
+private:
+    int32_t _pipe_fd[2]; //_pipe_fd[0]:读, _pipe_fd[1]:写
 
 };
 
